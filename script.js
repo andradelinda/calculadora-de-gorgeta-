bbc67c9f37a588ca564e5a1f371074e578c783d2
Mariana Silva Andrade <andrade.mariana@eaportal.org>
2024-03-27 09:52:42 -0300

    sétimo commit do HTML e o primeiro do JS

---
+++ script.js
@@ -0,0 +1,28 @@
+let conta = 0
+let pessoas = 0
+
+const containput = document.querySelector("#conta")
+containput.addEventListener("input", receberValorConta)
+
+function receberValorConta(evento) {
+    conta = Number(evento.target.value)
+
+}
+
+const pessoasInput = document.querySelector("#pessoas")
+pessoasInput.addEventListener("input",receberQuantidadePessoas)
+
+function receberQuantidadePessoas(evento) {
+    const paragrafoErro = document.querySelector(".pessoas #erro")
+    const divErro = document.querySelector(".pessoas .input-box")
+
+   if(evento.target.value === "0") {
+     paragrafoErro.style.display = "block"
+     divErro.setAttribute("id", "erro-div")
+   } else {
+    paragrafoErro.style.display = "none"
+    divErro.setAttribute("id", "")
+    pessoas = Number(evento.target.value)
+   }
+ }
+
